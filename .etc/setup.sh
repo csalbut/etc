133d1666131b3963591f3062b2aa6adc3bd4c689
Cezary Sałbut <csalbut@gmail.com>
2015-05-31 16:05:54 +0200

    add informal config guideline for fresh linux installations

--- .etc/setup.sh
+++ .etc/setup.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
 ln -s ~/.etc/tmux.conf ~/.tmux.conf
+
+# TODO on a fresh machine:
+# create user cs
+# install sudo
+# add user cs to sudoers: visudo
+#
+# as cs:
+# install git, zsh, vim
+# chsh -s /bin/zsh cs
+# start zsh, do not create .zshrc
+# clone vcsh, link to /usr/bin
+# pull all vcsh repos
+# pull vcsh-modules
+# vcsh <repo> submodule update --init
+#
